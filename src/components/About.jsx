d2527220cf0b9aeacb4b7481c18039281fab6098
Ashish Mishra <83667639+Ashish-mishra7011@users.noreply.github.com>
2024-08-07 00:52:36 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer';
 const ButtonLink = () => {
   return (
     <a
-      href="/Ashish_Mishra_ADGIPS.pdf"
+      href="/ASHISH_MISHRA_ADGIPS_01515607222.pdf"
       className='transition ease-in-out duration-300 p-3 rounded-md text-white border-2 hover:bg-white hover:text-black hover:scale-110 hover:-translate-y-1 cursor-pointer'
       
       download="Ashish_Mishra_ADGIPS.pdf"
